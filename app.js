777c63c6467a0fea24653688f98912b0ca656bfa
ColbyLanier <89653705+ColbyLanier@users.noreply.github.com>
2022-11-21 22:27:42 -0500

    added hw files

---
+++ app.js
@@ -0,0 +1,57 @@
+import "./env.js";
+
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import logger from "morgan";
+import path from "node:path";
+import {fileURLToPath} from "node:url";
+
+import blogRouter from "./routes/blog.js";
+import indexRouter from "./routes/index.js";
+
+const app = express();
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
+
+let authStatus = false;
+
+app.use("/", indexRouter);
+app.use(
+  "/blog",
+  (req, res, next) => {
+    console.log("1")
+    if (!authStatus) {
+      console.log("2");
+      res.redirect("/auth");
+    }
+    next();
+  },
+  blogRouter
+);
+
+app.use("/auth", (req, res) => {
+  console.log("3")
+  res.redirect('https://github.com/login/oauth/author/ize?client_id=${process.env.CLIENT_ID}');
+});
+
+app.get("/callback", ({query: {code}}, res) => {
+  const body = {client_id: process.env.CLIENT_ID,
+  client_secret: SECRET,
+  query};
+  const options = {headers:{accept:'application/json'}};
+  axios.post('https://github.com/login/oauth/access_token', body, options)
+    .then((res) => res.data.access_token)
+    .then((token) => {
+      state = true;
+      res.redirect("/blog");
+    })
+    .catch((err) => res.status(500).json({err: err.message}));
+})
+
+export default app;
